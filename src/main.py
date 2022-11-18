400bbd195f0a4d3b2dcd6dc12c018e9703486198
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2022-11-18 19:51:35 +0200

    Process

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
+STATE_STOP = 0
+STATE_RUN = 1
+STATE_PAUSE = 2
+
+app_state = STATE_STOP
+
+
 def main():
-    pass
+    while app_state == STATE_RUN:
+        if app_state == STATE_STOP:
+            break
 
 
 if __name__ == '__main__':
---
+++ src/process.py
@@ -0,0 +1,34 @@
+def process(delta: float) -> None:
+    print(delta)
+
+# current_sec = time.time()
+# next_sec = current_sec + 1
+# print('-', current_sec)
+#
+#
+# while current_sec < next_sec:
+#     current_sec = time.time()
+#     print(current_sec)
+#
+# print('-', current_sec)
+
+# i = 0
+# while current_sec < next_sec:
+#     i += 1
+#     current_sec = time.time()
+#     print(i)
+# print(i, time.time())
+
+
+# i = 0
+# while i < 1000:
+# i += 1
+# delta = time.time()
+# print(i, delta)
+
+# class Node:
+#     def process(self, delta: float) -> None:
+#         print(delta)
+
+# node = Node()
+# node.process(3)
