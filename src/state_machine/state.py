f8b7cce6177a17272b1768aab221d39f820afe52
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2022-11-18 14:15:06 +0200

    Project structure

--- src/state_machine/state.py
+++ src/state_machine/state.py
@@ -1,33 +1,34 @@
 class State:
-	state_machine = None
-	state_parent = None
+    state_machine = None
+    state_parent = None
 
-	def _ready():
-		yield (owner, "ready")
-		state_parent = get_parent()
-		assert state_parent != None, 'Hello'
+    def __init__(self, state):
+        # yield (owner, "ready")
+        # state_parent = get_parent()
+        # assert state_parent != None, 'Hello'
+        pass
 
-	def input(_event) -> None:
-		pass
+    def input(self, _event) -> None:
+        return
 
-	def unhandled_input(_event) -> None:
-		pass
+    def unhandled_input(self, _event) -> None:
+        return
 
-	def unhandled_key_input(_event) -> None:
-		pass
+    def unhandled_key_input(self, _event) -> None:
+        return
 
-	def process(_delta: float) -> None:
-		pass
+    def process(self, _delta: float) -> None:
+        return
 
-	def physics_process(_delta: float) -> None:
-		pass
+    def physics_process(self, _delta: float) -> None:
+        return
 
-	# Called by the state machine upon changing the active state. The `msg` parameter
-	# is a dictionary with arbitrary data the state can use to initialize itself.
-	def enter(_msg: dict = {}) -> None:
-		pass
+    # Called by the state machine upon changing the active state. The `msg` parameter
+    # is a dictionary with arbitrary data the state can use to initialize itself.
+    def enter(self, _msg: dict = {}) -> None:
+        return
 
-	# Called by the state machine before changing the active state. Use this function
-	# to clean up the state.
-	def exit() -> None:
-		pass
+    # Called by the state machine before changing the active state. Use this function
+    # to clean up the state.
+    def exit(self) -> None:
+        return
